02dbea871123d9aad11574488bd2c5242d9e6e01
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-01-18 13:50:30 +0530

    chore: eslint & format

--- bowling.js
+++ bowling.js
@@ -1,47 +1,47 @@
 function getFrames(rolls) {
-	if (!Array.isArray(rolls) || rolls.some(isNaN))
-		throw TypeError('Unsupported Type')
+  if (!Array.isArray(rolls) || rolls.some(isNaN))
+    throw TypeError('Unsupported Type');
 
-	const frames = []
+  const frames = [];
 
-	for (let i = 0; i < rolls.length; i += 2) {
-		if (i + 1 < rolls.length && (rolls[i] == 10 || rolls[i + 1] == 10)) {
-			// strike
-			frames.push([rolls[i], rolls[i + 1], rolls[i + 2]]);
-			i -= rolls[i] == 10;
-		}
-		else if (i + 1 < rolls.length && rolls[i] + rolls[i + 1] == 10) {
-			// spare
-			frames.push([rolls[i], rolls[i + 1], rolls[i + 2]]);
-		}
-		else if (i + 1 < rolls.length && rolls[i] + rolls[i + 1] < 10) {
-			// open
-			frames.push([rolls[i], rolls[i + 1]]);
-		}
-		else throw Error('Invalid Input')
+  for (let i = 0; i < rolls.length; i += 2) {
+    if (i + 1 < rolls.length && (rolls[i] == 10 || rolls[i + 1] == 10)) {
+      // strike
+      frames.push([rolls[i], rolls[i + 1], rolls[i + 2]]);
+      i -= rolls[i] == 10;
+    }
+    else if (i + 1 < rolls.length && rolls[i] + rolls[i + 1] == 10) {
+      // spare
+      frames.push([rolls[i], rolls[i + 1], rolls[i + 2]]);
+    }
+    else if (i + 1 < rolls.length && rolls[i] + rolls[i + 1] < 10) {
+      // open
+      frames.push([rolls[i], rolls[i + 1]]);
+    }
+    else throw Error('Invalid Input');
 
-		if (frames.length == 10)
-			break;
-	}
+    if (frames.length == 10)
+      break;
+  }
 
-	return frames;
+  return frames;
 }
 
 function getScores(rolls) {
-	return getFrames(rolls).map(listOfScores => listOfScores.reduce((acc, item) => acc + item, 0))
+  return getFrames(rolls).map(listOfScores => listOfScores.reduce((acc, item) => acc + item, 0));
 }
 
 function getScore(rolls) {
-	return getScores(rolls).reduce((acc, score) => acc + score);
+  return getScores(rolls).reduce((acc, score) => acc + score);
 }
 
 function getBestScore(rolls) {
-	return Math.max(...getScores(rolls));
+  return Math.max(...getScores(rolls));
 }
 
 module.exports = {
-	getFrames,
-	getScores,
-	getScore
-}
+  getFrames,
+  getScores,
+  getScore
+};
 
--- bowling.test.js
+++ bowling.test.js
@@ -1,30 +1,30 @@
 const {getFrames, getScore} = require('./bowling');
 
 describe('getScore', () => {
-	test('1', () => {
-		expect(getScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
-	})
+  test('1', () => {
+    expect(getScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
+  });
 
-	test('2', () => {
-		expect(getScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toBe(30);
-	})
+  test('2', () => {
+    expect(getScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toBe(30);
+  });
 
-	test('3', () => {
-		expect(getScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(16);
-	})
+  test('3', () => {
+    expect(getScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(16);
+  });
 
-	test('error when not an array', () => {
-		expect(() => {getScore('a')}).toThrow(TypeError);
-	})
+  test('error when not an array', () => {
+    expect(() => {getScore('a');}).toThrow(TypeError);
+  });
 
-	test('error when not an array of numbers', () => {
-		expect(() => {getScore(['a'])}).toThrow(TypeError);
-	})
+  test('error when not an array of numbers', () => {
+    expect(() => {getScore(['a']);}).toThrow(TypeError);
+  });
 
-	test('error when invalid game / inputs', () => {
-		expect(() => {getScore([6, 4, 3, 0, 0])}).toThrow(Error);
-	})
-})
+  test('error when invalid game / inputs', () => {
+    expect(() => {getScore([6, 4, 3, 0, 0]);}).toThrow(Error);
+  });
+});
 
 // describe('getFrames', () => {
 // 	test('_', () => {
